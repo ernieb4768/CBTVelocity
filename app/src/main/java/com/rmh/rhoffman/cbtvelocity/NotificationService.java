777f1011be77702de04deb3c91fe5375c0b4d8b1
Ryan Hoffman <rhoffman@Ryans-iMac.local>
2016-09-07 23:14:48 -0400

    add simple firebase push notifications

--- app/src/main/java/com/rmh/rhoffman/cbtvelocity/NotificationService.java
+++ app/src/main/java/com/rmh/rhoffman/cbtvelocity/NotificationService.java
@@ -1,66 +1,25 @@
 package com.rmh.rhoffman.cbtvelocity;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.util.Log;
 
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 /**
  * Adds notifications to the app.
  */
 
-// FIXME: No notifications working currently
-public class NotificationService {
+public class NotificationService extends FirebaseMessagingService {
 
-	private static String TITLE;
-	private static String CONTENT;
+	private static final String TAG = "FCM Service";
 
-	// If this constructor is called there will be additional setup needed to create the notification.
-	// The setNotificationTitle() and setNotificationContent() methods will be needed then you will need to call createNotification().
-	public NotificationService(){
-		// Empty constructor.
+	@Override
+	public void onMessageReceived(RemoteMessage remoteMessage) {
+		// TODO: Handle FCM messages here.
+		// If the application is in the foreground handle both data and notification messages here.
+		// Also if you intend on generating your own notifications as a result of a received FCM
+		// message, here is where that should be initiated.
+		Log.d(TAG, "From: " + remoteMessage.getFrom());
+		Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
 	}
 
-	/**
-	 * Create a notification and set it to go off at the appropriate time.
-	 */
-	public static void createNotification(){
-
-		// Set up the notification manager and create the intents to handle the action when clicked.
-		NotificationManager notification = (NotificationManager) App.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
-		Intent intent1 = new Intent(App.getContext(), MainActivity.class);
-		PendingIntent pendingIntent1 = PendingIntent.getActivity(App.getContext(), 0, intent1, 0);
-
-		// Build the notification and set it to go off.
-		Notification notify = new Notification.Builder(App.getContext())
-				.setContentTitle(TITLE)
-				.setContentText(CONTENT)
-				.setSmallIcon(R.mipmap.ic_v_notification)
-				.setContentIntent(pendingIntent1)
-				.setSound(ringTone())
-				.build();
-
-		notification.notify(1, notify);
-	}
-
-	/**
-	 * Get the default notification tone to be played with the notification goes off.
-	 *
-	 * @return Uri
-	 */
-	private static Uri ringTone(){
-		Uri tone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-		return tone;
-	}
-
-	public static void setNotificationTitle(String title){
-		TITLE = title;
-	}
-
-	public static void setNotificationContent(String content){
-		CONTENT = content;
-	}
 }
